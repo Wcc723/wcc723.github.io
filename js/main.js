ff004841d478f27268d1a438f8911beeba1d5956
wcc723 <agito723@gmail.com>
2017-12-06 11:16:03 +0800

    Site updated: 2017-12-06 11:16:00

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
     });
 
     // Caption
-    $('.article-entry').each(function(i) {
+    $('.article-entry .need-gallery').each(function(i) {
         $(this).find('img').each(function() {
             if (this.alt) {
                 $(this).after('<span class="caption">' + this.alt + '</span>');
